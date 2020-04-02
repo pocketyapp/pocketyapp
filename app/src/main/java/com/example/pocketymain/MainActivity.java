140996c0e130d39e1abb17a048d1337aaf357bbd
TheCrisHunter <62511319+TheCrisHunter@users.noreply.github.com>
2020-04-02 20:10:07 +0200

    Login y Register conectados

---
+++ app/src/main/java/com/example/pocketymain/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.pocketymain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void abrirRegistro(View view){
+        Intent intent=new Intent(this,Register.class);
+        startActivity(intent);
+
+    }
+}
