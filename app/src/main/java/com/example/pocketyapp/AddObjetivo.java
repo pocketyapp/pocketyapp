cabbbc927de63b493b63d28cab6fab5af21d0c24
msertor <51539604+msertor@users.noreply.github.com>
2020-04-05 18:24:29 +0200

    Android Manifest actualización + Añadir objetivo actualización

--- app/src/main/java/com/example/pocketyapp/AddObjetivo.java
+++ app/src/main/java/com/example/pocketyapp/AddObjetivo.java
@@ -5,16 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class AddObjetivo extends AppCompatActivity {
 
     Button btcancel;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_objetivo);
+        
 
         btcancel = findViewById(R.id.button_cancelar);
         btcancel.setOnClickListener(new View.OnClickListener(){
@@ -31,4 +38,16 @@ public class AddObjetivo extends AppCompatActivity {
         Intent intent = new Intent(this, MainMenu.class);
         startActivity(intent);
     }
+
+    /*public void onBtnClick(){
+        btguardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String result = et.getText().toString();
+                arrayList.add(result);
+                adapter.notifyDataSetChanged();
+
+            }
+        });
+    }*/
 }
--- app/src/main/java/com/example/pocketyapp/MainMenu.java
+++ app/src/main/java/com/example/pocketyapp/MainMenu.java
@@ -3,26 +3,23 @@ package com.example.pocketyapp;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+
 
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
+
+
 
 public class MainMenu extends AppCompatActivity {
 
-    EditText et;
-    Button bt;
-    ListView lv;
+        Button bt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
 
         bt = findViewById(R.id.button_addObjective);
         bt.setOnClickListener(new View.OnClickListener() {
@@ -31,10 +28,6 @@ public class MainMenu extends AppCompatActivity {
                 openaddObjetivoView();
             }
         });
-
-        et = (EditText) findViewById(R.id.editText);
-
-
 
     }
 
