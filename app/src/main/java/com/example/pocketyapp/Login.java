1343f1f531ca493f4b3a014be788597b63731cd1
msertor <51539604+msertor@users.noreply.github.com>
2020-04-08 09:50:57 +0200

    Merge de el rebase de Cristian a falta de actualizar Register.java en conexión con firebase

--- app/src/main/java/com/example/pocketyapp/Login.java
+++ app/src/main/java/com/example/pocketyapp/Login.java
@@ -2,14 +2,9 @@ package com.example.pocketyapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class Login extends AppCompatActivity {
 
