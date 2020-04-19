b4a73d4d0897f6ccc3bab1b84b4341d397ede64a
msertor <51539604+msertor@users.noreply.github.com>
2020-04-19 19:01:02 +0200

    Añadir objetivos a tu cuenta de la base de datos

--- app/src/main/java/com/example/pocketyapp/AddObjetivo.java
+++ app/src/main/java/com/example/pocketyapp/AddObjetivo.java
@@ -1,21 +1,31 @@
 package com.example.pocketyapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
+import android.widget.Toast;
 
-import java.util.ArrayList;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 public class AddObjetivo extends AppCompatActivity {
 
-    Button btcancel;
-    EditText etNombre;
+    Button btcancel, btguardar;
+    EditText etNombre, etCantidad;
+    FirebaseAuth mAuth;
+    DatabaseReference reff;
+    Objetivo objetivo;
+
 
 
     @Override
@@ -25,8 +35,28 @@ public class AddObjetivo extends AppCompatActivity {
 
         etNombre = findViewById(R.id.editText_NombreObjetivo);
         etNombre.requestFocus();
-
+        etCantidad = findViewById(R.id.editText_CantidadObjetivo);
+        btguardar = findViewById(R.id.button_guardarObjetivo);
         btcancel = findViewById(R.id.button_cancelar);
+
+        objetivo=new Objetivo();
+        mAuth = FirebaseAuth.getInstance();
+        reff= FirebaseDatabase.getInstance().getReference();
+
+        btguardar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                int cantidadObjetivo = Integer.parseInt(etCantidad.getText().toString().trim());
+                objetivo.setName(etNombre.getText().toString().trim());
+                objetivo.setQuantity(cantidadObjetivo);
+                String id = mAuth.getCurrentUser().getUid();
+
+                reff.child("Cuentas").child(id).child("Objetivos").push().setValue(objetivo);
+                Toast.makeText(AddObjetivo.this, "Objetivo añadido correctamente", Toast.LENGTH_LONG).show();
+            }
+
+        });
+
         btcancel.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -42,15 +72,5 @@ public class AddObjetivo extends AppCompatActivity {
         startActivity(intent);
     }
 
-    /*public void onBtnClick(){
-        btguardar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String result = et.getText().toString();
-                arrayList.add(result);
-                adapter.notifyDataSetChanged();
 
-            }
-        });
-    }*/
 }
---
+++ app/src/main/java/com/example/pocketyapp/Objetivo.java
@@ -0,0 +1,25 @@
+package com.example.pocketyapp;
+
+public class Objetivo {
+    private String Name;
+    private int Quantity;
+
+    public Objetivo() {
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getQuantity() {
+        return Quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        Quantity = quantity;
+    }
+}
